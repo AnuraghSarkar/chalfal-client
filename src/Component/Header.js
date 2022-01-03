387e5610e89f29a3627490ae071b3c26660e71c0
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-03 21:43:01 +0545

    Working on Log/Reg dropdown UI

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -6,7 +6,9 @@ import {
   PlusIcon,
   SearchIcon,
   ChevronDownIcon,
+  UserIcon,
 } from "@heroicons/react/outline";
+import Button from "./Button";
 
 const Header = () => {
   return (
@@ -24,7 +26,7 @@ const Header = () => {
             className="bg-chalfal_color-brighter text-sm p-1 pl-2 pr-0 focus:outline-none block text-white"
           />
         </form>
-        <button className="px-2 py-1">
+        {/* <button className="px-2 py-1">
           <ChatIcon className="text-gray-300 h-6 w-6 mx-2" />
         </button>
         <button className="px-2 py-1">
@@ -32,19 +34,27 @@ const Header = () => {
         </button>
         <button className="px-2 py-1">
           <PlusIcon className="text-gray-300 h-6 w-6 mx-2" />
-        </button>
+        </button> */}
+        <div className="mx-2">
+          <Button outline className="mr-1">
+            Log In
+          </Button>
+          <Button className="">Register</Button>
+        </div>
 
         <button className="rounded-md flex ml-4">
-          <div className="w-8 h-8 bg-gray-600 rounded-md">
+          <UserIcon className="w-6 h-6 text-gray-400 m-1 border border-gray-700"/>
+
+          {/* <div className="w-8 h-8 bg-gray-600 rounded-md">
             <img
               src={Avatar}
               alt="Avatar"
               style={{ filter: "invert(100)" }}
               className="block"
             />
-          </div>
+          </div> */}
 
-          <ChevronDownIcon className="text-gray-500 h-5 w-5 mt-2 ml-0" />
+          <ChevronDownIcon className="text-gray-500 h-5 w-5 mt-2 m-1" />
         </button>
       </div>
     </header>
