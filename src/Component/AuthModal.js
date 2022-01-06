f779ac3e45ff26d1dea61de92ed08f9d7bb89f2c
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-06 17:50:25 +0545

    Using Context for storing user and implementing conditional rendering

--- src/Component/AuthModal.js
+++ src/Component/AuthModal.js
@@ -2,127 +2,140 @@ import Input from "./Input";
 import Button from "./Button";
 import { useState, useContext } from "react";
 import axios from "axios";
-import AuthModalContext from "./AuthModalContext";
+import AuthModalContext from "../Store/AuthModalContext";
+import ClickOutHandler from "react-clickout-handler";
+import UserContext from "../Store/UserContext";
 
-const AuthModal = (props) => {
+const AuthModal = () => {
   const [modalType, setModalType] = useState("login");
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const modalContext = useContext(AuthModalContext);
+  const user = useContext(UserContext);
 
   const visibleClass = modalContext.show ? "block" : "hidden";
+
+  if (modalContext.show && modalContext.show !== modalType) {
+    setModalType(modalContext.show);
+  }
 
   const register = async (e) => {
     e.preventDefault();
     const data = { email, username, password };
     axios.post("http://localhost:4000/register", data, {
       withCredentials: true,
-    });
+    }).then(()=>{user.setUsername({username})});
   };
 
   return (
     <div
       className={
-        "w-screen h-screen fixed z-20 top-0 left-0 flex" + visibleClass
+        "w-screen h-screen fixed z-20 top-0 left-0 flex " + visibleClass
       }
       style={{ backgroundColor: "rgba(0,0,0,.6" }}
     >
-      <div className="border border-chalfal_border w-3/4  sm:w-1/2 md:w-1/2 bg-chalfal_color p-5 text-chalfal_text self-center mx-auto rounded-md">
-        {modalType === "login" ? (
-          <h1 className="text-2xl mb-5">Login</h1>
-        ) : (
-          <h1 className="text-2xl mb-5">Sign Up</h1>
-        )}
-        {modalType === "login" ? (
-          <>
-            <label>
-              <span className="text-chalfal_text-darker text-sm">
-                Username:
-              </span>
-              <Input type="text" className="mb-3 w-full" />
-            </label>
-            <label>
-              <span className="text-chalfal_text-darker text-sm">
-                Password:
-              </span>
-              <Input type="password" className="mb-3 w-full" />
-            </label>
-          </>
-        ) : (
-          <>
-            <label>
-              <span className="text-chalfal_text-darker text-sm">Email:</span>
-              <Input
-                type="email"
-                className="mb-3 w-full"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </label>
-            <label>
-              <span className="text-chalfal_text-darker text-sm">
-                Username:
-              </span>
-              <Input
-                type="text"
-                className="mb-3 w-full"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </label>
-            <label>
-              <span className="text-chalfal_text-darker text-sm">
-                Password:
-              </span>
-              <Input
-                type="password"
-                className="mb-3 w-full"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </label>
-          </>
-        )}
-        {modalType === "login" && (
-          <Button className="w-full mb-3" style={{ borderRadius: ".4rem" }}>
-            Log In
-          </Button>
-        )}
+      <ClickOutHandler
+        onClickOut={() => {
+          modalContext.setShow(false);
+        }}
+      >
+        <div className="border border-chalfal_border w-3/4  sm:w-1/2 md:w-1/2 bg-chalfal_color p-5 text-chalfal_text self-center mx-auto rounded-md">
+          {modalType === "login" ? (
+            <h1 className="text-2xl mb-5">Login</h1>
+          ) : (
+            <h1 className="text-2xl mb-5">Sign Up</h1>
+          )}
+          {modalType === "login" ? (
+            <>
+              <label>
+                <span className="text-chalfal_text-darker text-sm">
+                  Username:
+                </span>
+                <Input type="text" className="mb-3 w-full" />
+              </label>
+              <label>
+                <span className="text-chalfal_text-darker text-sm">
+                  Password:
+                </span>
+                <Input type="password" className="mb-3 w-full" />
+              </label>
+            </>
+          ) : (
+            <>
+              <label>
+                <span className="text-chalfal_text-darker text-sm">Email:</span>
+                <Input
+                  type="email"
+                  className="mb-3 w-full"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </label>
+              <label>
+                <span className="text-chalfal_text-darker text-sm">
+                  Username:
+                </span>
+                <Input
+                  type="text"
+                  className="mb-3 w-full"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </label>
+              <label>
+                <span className="text-chalfal_text-darker text-sm">
+                  Password:
+                </span>
+                <Input
+                  type="password"
+                  className="mb-3 w-full"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </label>
+            </>
+          )}
+          {modalType === "login" && (
+            <Button className="w-full mb-3" style={{ borderRadius: ".4rem" }}>
+              Log In
+            </Button>
+          )}
 
-        {modalType === "register" && (
-          <Button
-            className="w-full mb-3"
-            onClick={(e) => register(e)}
-            style={{ borderRadius: ".4rem" }}
-          >
-            Sign Up
-          </Button>
-        )}
-
-        {modalType === "login" ? (
-          <div>
-            New to Chalfal?
-            <button
-              className="text-blue-600 ml-2"
-              onClick={() => setModalType("register")}
+          {modalType === "register" && (
+            <Button
+              className="w-full mb-3"
+              onClick={(e) => register(e)}
+              style={{ borderRadius: ".4rem" }}
             >
               Sign Up
-            </button>
-          </div>
-        ) : (
-          <div>
-            Already a member?
-            <button
-              className="text-blue-600 ml-2"
-              onClick={() => setModalType("login")}
-            >
-              Log In
-            </button>
-          </div>
-        )}
-      </div>
+            </Button>
+          )}
+
+          {modalType === "login" ? (
+            <div>
+              New to Chalfal?
+              <button
+                className="text-blue-600 ml-2"
+                onClick={() => modalContext.setShow("register")}
+              >
+                Sign Up
+              </button>
+            </div>
+          ) : (
+            <div>
+              Already a member?
+              <button
+                className="text-blue-600 ml-2"
+                onClick={() => modalContext.setShow("login")}
+              >
+                Log In
+              </button>
+            </div>
+          )}
+        </div>
+      </ClickOutHandler>
     </div>
   );
 };
--- src/Component/Header.js
+++ src/Component/Header.js
@@ -11,8 +11,9 @@ import {
   UserIcon,
 } from "@heroicons/react/outline";
 import Button from "./Button";
-
-import { useState } from "react";
+import AuthModalContext from "../Store/AuthModalContext";
+import UserContext from "../Store/UserContext";
+import { useState, useContext } from "react";
 import ClickOutHandler from "react-clickout-handler";
 
 const Header = () => {
@@ -25,6 +26,8 @@ const Header = () => {
       setUserDropdownVisibilityClass("hidden");
     }
   };
+  const authModal = useContext(AuthModalContext);
+  const user = useContext(UserContext);
   return (
     <header className="w-full bg-chalfal_color p-2">
       <div className="mx-4 flex relative">
@@ -40,21 +43,42 @@ const Header = () => {
             className="bg-chalfal_color-brighter text-sm p-1 pl-2 pr-0 focus:outline-none block text-white"
           />
         </form>
-        {/* <button className="px-2 py-1">
-          <ChatIcon className="text-gray-300 h-6 w-6 mx-2" />
-        </button>
-        <button className="px-2 py-1">   
-          <BellIcon className="text-gray-300 h-6 w-6 mx-2" />
-        </button>
-        <button className="px-2 py-1">
-          <PlusIcon className="text-gray-300 h-6 w-6 mx-2" />
-        </button> */}
-        <div className="mx-2 hidden sm:block">
-          <Button outline className="mr-1 h-8">
-            Log In
-          </Button>
-          <Button className="h-8">Sign Up</Button>
-        </div>
+        {user.username && (
+          <>
+            
+            <button className="px-2 py-1">
+              <ChatIcon className="text-gray-300 h-6 w-6 mx-2" />
+            </button>
+            <button className="px-2 py-1">
+              <BellIcon className="text-gray-300 h-6 w-6 mx-2" />
+            </button>
+            <button className="px-2 py-1">
+              <PlusIcon className="text-gray-300 h-6 w-6 mx-2" />
+            </button>
+          </>
+        )}
+
+        {!user.username && (
+          <div className="mx-2 hidden sm:block">
+            <Button
+              outline
+              className="mr-1 h-8"
+              onClick={() => {
+                authModal.setShow("login");
+              }}
+            >
+              Log In
+            </Button>
+            <Button
+              className="h-8"
+              onClick={() => {
+                authModal.setShow("register");
+              }}
+            >
+              Sign Up
+            </Button>
+          </div>
+        )}
 
         <ClickOutHandler
           onClickOut={() => setUserDropdownVisibilityClass("hidden")}
@@ -63,32 +87,49 @@ const Header = () => {
             className="rounded-md flex ml-4"
             onClick={toggleUserDropdownVisibility}
           >
-            <UserIcon className="w-6 h-6 text-gray-400 m-1 border border-gray-700" />
-
-            {/* <div className="w-8 h-8 bg-gray-600 rounded-md">
-            <img
-              src={Avatar}
-              alt="Avatar"
-              style={{ filter: "invert(100)" }}
-              className="block"
-            />
-          </div> */}
+            {!user.username && (
+              <UserIcon className="w-6 h-6 text-gray-400 m-1 border border-gray-700" />
+            )}
+            {user.username && (
+              <div className="w-8 h-8 bg-gray-600 rounded-md">
+                <img
+                  src={Avatar}
+                  alt="Avatar"
+                  style={{ filter: "invert(100)" }}
+                  className="block"
+                />
+              </div>
+            )}
 
             <ChevronDownIcon className="text-gray-500 h-5 w-5 mt-2 m-1" />
           </button>
-        </ClickOutHandler>
 
-        <div
-          className={
-            "absolute right-0 top-8 bg-chalfal_color border border-gray-700 z-10 rounded-md text-chalfal_text overflow-hidden " +
-            userDropdownVisibilityClass
-          }
-        >
-          <button className="flex w-50 py-2 px-3 hover:bg-gray-300 hover:text-black text-sm">
-            <LoginIcon className="w-5 h-5 mr-2" />
-            Log In / Sign Up
-          </button>
-        </div>
+          <div
+            className={
+              "absolute right-0 top-8 bg-chalfal_color border border-gray-700 z-10 rounded-md text-chalfal_text overflow-hidden " +
+              userDropdownVisibilityClass
+            }
+          >
+            {!user.username && (
+              <button
+                className="flex w-50 py-2 px-3 hover:bg-gray-300 hover:text-black text-sm"
+                onClick={() => authModal.setShow("login")}
+              >
+                <LoginIcon className="w-5 h-5 mr-2" />
+                Log In / Sign Up
+              </button>
+            )}
+            {user.username && (
+              <button
+                className="flex w-50 py-2 px-3 hover:bg-gray-300 hover:text-black text-sm"
+                onClick={() => user.logout()}
+              >
+                <LogoutIcon className="w-5 h-5 mr-2" />
+                Log Out {user.username}
+              </button>
+            )}
+          </div>
+        </ClickOutHandler>
       </div>
     </header>
   );
