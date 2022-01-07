bfd42dc7fe152932e1e17c816ce57d1945c620a2
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-07 23:18:04 +0545

    Optimizing AuthModal

--- src/Component/AuthModal.js
+++ src/Component/AuthModal.js
@@ -52,81 +52,115 @@ function AuthModal() {
       style={{ backgroundColor: "rgba(0,0,0,.6)" }}
     >
       <ClickOutHandler onClickOut={() => modalContext.setShow(false)}>
-        <div className="border border-chalfal_color-brightest w-3/4 sm:w-1/2 lg:w-1/4 bg-chalfal_color p-5 text-chalfal_text self-center mx-auto rounded-md">
-          {modalType === "login" && <h1 className="text-2xl mb-5">Login</h1>}
-          {modalType === "register" && (
-            <h1 className="text-2xl mb-5">Sign Up</h1>
-          )}
-          {modalType === "register" && (
-            <label>
-              <span className="text-chalfal_text-darker text-sm">E-mail:</span>
-              <Input
-                type="email"
-                className="mb-3 w-full"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </label>
-          )}
-          <label>
-            <span className="text-chalfal_text-darker text-sm">Username:</span>
-            <Input
-              type="text"
-              className="mb-3 w-full"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </label>
+        <div class=" p-6 m-auto w-3/4 sm:w-1/2 lg:w-1/4 bg-gray-800 rounded-md shadow-md ">
+          <h1 class="text-3xl font-semibold text-center text-white">
+            {modalType === "login" ? "Login" : "Register"}
+          </h1>
 
-          <label>
-            <span className="text-chalfal_text-darker text-sm">Password:</span>
-            <Input
-              type="password"
-              className="mb-3 w-full"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
-          {modalType === "login" && (
-            <Button
-              className="w-full mb-3"
-              style={{ borderRadius: ".3rem" }}
-              onClick={() => login()}
-            >
-              Log In
-            </Button>
-          )}
-          {modalType === "register" && (
-            <Button
-              className="w-full mb-3"
-              style={{ borderRadius: ".3rem" }}
-              onClick={(e) => register(e)}
-            >
-              Sign Up
-            </Button>
-          )}
-
-          {modalType === "login" && (
+          <form class="mt-6">
+            {modalType === "register" && (
+              <div>
+                <label for="username" class="block text-sm text-gray-200">
+                  Email
+                </label>
+                <Input
+                  type="email"
+                  className="mb-3 w-full"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            )}
             <div>
-              New to chalfal?{" "}
+              <label for="username" class="block text-sm mt-4 text-gray-200">
+                Username
+              </label>
+              <Input
+                type="text"
+                className="mb-3 w-full"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+
+            <div class="mt-4">
+              <div class="flex items-center justify-between">
+                <label for="password" class="block text-sm text-gray-200">
+                  Password
+                </label>
+              </div>
+
+              <Input
+                type="password"
+                className="mb-3 w-full"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <div class="mt-6">
+              {modalType === "login" ? (
+                <button
+                  class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+                >
+                  Log In
+                </button>
+              ) : (
+                <button
+                  class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+                >
+                  Sign Up
+                </button>
+              )}
+            </div>
+          </form>
+
+          <div class="flex items-center justify-between mt-4">
+            <span class="w-1/5 border-b border-gray-400 lg:w-1/5"></span>
+
+            <p class="text-xs text-center  uppercase text-gray-400 hover:underline">
+              or {modalType === "login" ? "Login" : "Register"} with Google
+            </p>
+
+            <span class="w-1/5 border-b border-gray-400 lg:w-1/5"></span>
+          </div>
+
+          <div class="flex items-center mt-6 -mx-2">
+            <button
+              type="button"
+              class="flex items-center justify-center w-full px-6 py-2 mx-2 text-sm font-medium text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:bg-blue-400 focus:outline-none"
+            >
+              <svg class="w-4 h-4 mx-2 fill-current" viewBox="0 0 24 24">
+                <path d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z"></path>
+              </svg>
+
+              <span class="hidden mx-2 sm:inline">
+                Sign {modalType === "login" ? "in" : "up"} with Google
+              </span>
+            </button>
+          </div>
+          {modalType === "login" ? (
+            <p class="mt-8 text-xs font-light text-center text-gray-400">
+              Don't have an account?
               <button
-                className="text-blue-600"
+                href="/"
+                class="font-medium text-gray-200 hover:underline"
                 onClick={() => modalContext.setShow("register")}
               >
-                SIGN UP
+                Create One
               </button>
-            </div>
-          )}
-          {modalType === "register" && (
-            <div>
-              Already have an account?{" "}
+            </p>
+          ) : (
+            <p class="mt-8 text-xs font-light text-center text-gray-400">
+              Already have an account?
               <button
-                className="text-blue-600"
-                onClick={() => modalContext.setShow("login")}
+                href="/"
+                class="font-medium text-gray-200 hover:underline"
+                onClick={() => modalContext.setShow("login") }
               >
-                LOG IN
+                Register
               </button>
-            </div>
+            </p>
           )}
         </div>
       </ClickOutHandler>
