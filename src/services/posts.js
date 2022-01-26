88343bc5648873e24e9fb35a3eea0499b56bc00d
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-26 10:38:51 +0545

    added services for getting post comments upvote and downvotes

--- src/services/posts.js
+++ src/services/posts.js
@@ -31,12 +31,37 @@ const getSearchResults = async (query, limit, page) => {
   );
   return response.data;
 };
+
 const addNew = async (postObj) => {
   const response = await axios.post(`${baseUrl}`, postObj, setConfig());
   return response.data;
 };
+
 const editPost = async (id, postObj) => {
   const response = await axios.patch(`${baseUrl}/${id}`, postObj, setConfig());
+  return response.data;
+};
+
+const getPostComments = async (id) => {
+  const response = await axios.get(`${baseUrl}/${id}/comments`);
+  return response.data;
+};
+
+const upvotePost = async (id) => {
+  const response = await axios.post(
+    `${baseUrl}/${id}/upvote`,
+    null,
+    setConfig()
+  );
+  return response.data;
+};
+
+const downvotePost = async (id) => {
+  const response = await axios.post(
+    `${baseUrl}/${id}/downvote`,
+    null,
+    setConfig()
+  );
   return response.data;
 };
 
@@ -46,6 +71,9 @@ const postService = {
   getSearchResults,
   addNew,
   editPost,
+  getPostComments,
+  upvotePost,
+  downvotePost,
 };
 
 export default postService;
