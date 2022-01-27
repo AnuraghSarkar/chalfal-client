5a130d2cfce40a9e29134af3ee6ffb2da8cc818e
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-27 16:31:37 +0545

    :construction: working on user reducer

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,7 @@
+import authService from '../services/auth';
+import userService from '../services/user';
+import storageService from '../utils/localStorage';
+
+const userReducer = (state = null, action) => { };
+
+export default userReducer;
