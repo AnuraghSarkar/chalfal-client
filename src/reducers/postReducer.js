d219438a8d8abc579f537782013eed1f57517be2
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-27 16:43:48 +0545

    :construction: added action switch cases for posts

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,5 +1,29 @@
-import postService from '../services/posts';
+import postService from "../services/posts";
 
-const postReducer = (state = null, action) => { };
+const postReducer = (state = null, action) => {
+  switch (action.type) {
+    case "SET_POSTS":
+      return action.payload;
+    case "LOAD_MORE_POSTS":
+      return {
+        ...action.payload,
+        results: [...state.results, ...action.payload.results],
+      };
+    case "TOGGLE_VOTE":
+      return {
+        ...state,
+        results: state.results.map((r) =>
+          r.id !== action.payload.id ? r : { ...r, ...action.payload.data }
+        ),
+      };
+    case "DELETE_POST":
+      return {
+        ...state,
+        results: state.results.filter((r) => r.id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
 
 export default postReducer;
