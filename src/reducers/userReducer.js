cc6fdd5906c6b2ef9cd3a565ff85ac60aaef2c00
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-27 16:35:29 +0545

    :hammer: added cases for user action

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,7 +1,24 @@
-import authService from '../services/auth';
-import userService from '../services/user';
-import storageService from '../utils/localStorage';
+import authService from "../services/auth";
+import userService from "../services/user";
+import storageService from "../utils/localStorage";
 
-const userReducer = (state = null, action) => { };
+const userReducer = (state = null, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return action.payload;
+    case "SIGNUP":
+      return action.payload;
+    case "LOGOUT":
+      return null;
+    case "SET_USER":
+      return action.payload;
+    case "SET_AVATAR":
+      return { ...state, ...action.payload };
+    case "REMOVE_AVATAR":
+      return { ...state, avatar: { exists: false } };
+    default:
+      return state;
+  }
+};
 
 export default userReducer;
