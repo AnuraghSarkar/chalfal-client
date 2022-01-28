8511f47ce1cb5cb7ced17ae18f2dd6ada17efe4b
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-28 12:54:32 +0545

    :clown_face: dispatching the notification implementing duration

--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -9,4 +9,25 @@ const notificationReducer = (state = [], action) => {
   }
 };
 
+let timeoutID = null;
+
+export const notify = (message, severity) => {
+  const duration = severity === "error" ? 15 : 5;
+
+  return (dispatch) => {
+    clearTimeout(timeoutID);
+
+    dispatch({
+      type: "SET_NOTIFICATION",
+      payload: { message, severity },
+    });
+
+    timeoutID = setTimeout(() => {
+      dispatch({
+        type: "CLEAR_NOTIFICATION",
+      });
+    }, duration * 1000);
+  };
+};
+
 export default notificationReducer;
