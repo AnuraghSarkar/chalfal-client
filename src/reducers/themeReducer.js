e6dae425e83aa7857381f6936d93927e10170229
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-28 13:00:21 +0545

    :clown_face: dispatching toggller and setted dark mode

--- src/reducers/themeReducer.js
+++ src/reducers/themeReducer.js
@@ -1,3 +1,5 @@
+import storageService from "../utils/localStorage";
+
 const themeReducer = (state = false, action) => {
   switch (action.type) {
     case "TOGGLE_DARK_MODE":
@@ -5,6 +7,24 @@ const themeReducer = (state = false, action) => {
     default:
       return state;
   }
+};
+
+export const toggleDarkMode = (isDarkMode) => {
+  return (dispatch) => {
+    storageService.saveDarkMode(isDarkMode);
+
+    dispatch({ type: "TOGGLE_DARK_MODE" });
+  };
+};
+
+export const setDarkMode = () => {
+  return (dispatch) => {
+    const isDarkMode = storageService.loadDarkMode();
+
+    if (isDarkMode === "true") {
+      dispatch({ type: "TOGGLE_DARK_MODE" });
+    }
+  };
 };
 
 export default themeReducer;
