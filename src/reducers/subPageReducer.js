1e080b6ebd266b4fc990fb931a65e8df1983385f
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-28 09:36:49 +0545

    :green_heart: switch case for sub pages and posts func

--- src/reducers/subPageReducer.js
+++ src/reducers/subPageReducer.js
@@ -1,3 +1,38 @@
-const subPageReducer = (state = {}, action) => {};
+const subPageReducer = (state = null, action) => {
+  switch (action.type) {
+    case "FETCH_SUB":
+      return action.payload;
+    case "LOAD_SUB_POSTS":
+      return {
+        ...state,
+        posts: {
+          ...action.payload.posts,
+          results: [...state.posts.results, ...action.payload.posts.results],
+        },
+      };
+    case "TOGGLE_SUBPAGE_VOTE":
+      return {
+        ...state,
+        posts: {
+          ...state.posts,
+          results: state.posts.results.map((p) =>
+            p.id !== action.payload.id ? p : { ...p, ...action.payload.data }
+          ),
+        },
+      };
+    case "SUBSCRIBE_SUB":
+      return {
+        ...state,
+        subDetails: { ...state.subDetails, ...action.payload },
+      };
+    case "EDIT_DESCRIPTION":
+      return {
+        ...state,
+        subDetails: { ...state.subDetails, description: action.payload },
+      };
+    default:
+      return state;
+  }
+};
 
 export default subPageReducer;
