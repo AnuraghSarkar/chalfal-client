ae548acf695cc9ae613032328bb4337272a7a8be
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-28 09:27:35 +0545

    :fire: switch case model for subreddit func.

--- src/reducers/subReducer.js
+++ src/reducers/subReducer.js
@@ -1,3 +1,24 @@
-const subReducer = (state = {}, action) => {};
+const subReducer = (state = null, action) => {
+  switch (action.type) {
+    case "SET_ALL_SUBS_LIST":
+      return { ...state, allSubs: action.payload };
+    case "SET_TOP_SUBS_LIST":
+      return { ...state, topSubs: action.payload };
+    case "SUBSCRIBE_SUB_FROM_LIST":
+      return {
+        ...state,
+        topSubs: state.topSubs.map((t) =>
+          t.id !== action.payload.id ? t : { ...t, ...action.payload.data }
+        ),
+      };
+    case "ADD_NEW_SUB":
+      return {
+        ...state,
+        allSubs: [...state.allSubs, action.payload],
+      };
+    default:
+      return state;
+  }
+};
 
 export default subReducer;
