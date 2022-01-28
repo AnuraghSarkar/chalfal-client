86bcaaaadd0b1ec4baacb54291331925351b0954
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-28 12:46:53 +0545

    switch case for search func

--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -1,3 +1,22 @@
-const searchReducer = (state = {}, action) => { };
+const searchReducer = (state = null, action) => {
+  switch (action.type) {
+    case "SET_SEARCH_RESULTS":
+      return action.payload;
+    case "TOGGLE_SEARCH_VOTE":
+      return {
+        ...state,
+        results: state.results.map((r) =>
+          r.id !== action.payload.id ? r : { ...r, ...action.payload.data }
+        ),
+      };
+    case "LOAD_SEARCH_POSTS":
+      return {
+        ...action.payload,
+        results: [...state.results, ...action.payload.results],
+      };
+    default:
+      return state;
+  }
+};
 
 export default searchReducer;
