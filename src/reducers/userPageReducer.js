182ba6536cdfb2e89fb5ffdbf92bf5a8f387d84c
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-28 09:21:30 +0545

    reducer for user page

--- src/reducers/userPageReducer.js
+++ src/reducers/userPageReducer.js
@@ -1,3 +1,28 @@
-const userPageReducer = (state = null, action) => {};
+const userPageReducer = (state = null, action) => {
+  switch (action.type) {
+    case "FETCH_USER":
+      return action.payload;
+    case "TOGGLE_USERPAGE_VOTE":
+      return {
+        ...state,
+        posts: {
+          ...state.posts,
+          results: state.posts.results.map((r) =>
+            r.id !== action.payload.id ? r : { ...r, ...action.payload.data }
+          ),
+        },
+      };
+    case "LOAD_USER_POSTS":
+      return {
+        ...state,
+        posts: {
+          ...action.payload.posts,
+          results: [...state.posts.results, ...action.payload.posts.results],
+        },
+      };
+    default:
+      return state;
+  }
+};
 
 export default userPageReducer;
