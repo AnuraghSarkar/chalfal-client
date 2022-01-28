f195b5a1b6c7ddfbbb995c1c358af60ff03443f4
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-28 12:59:04 +0545

    :construction: toggling dark mode case

--- src/reducers/themeReducer.js
+++ src/reducers/themeReducer.js
@@ -1,3 +1,10 @@
-const themeReducer = (state = false, action) => {};
+const themeReducer = (state = false, action) => {
+  switch (action.type) {
+    case "TOGGLE_DARK_MODE":
+      return !state;
+    default:
+      return state;
+  }
+};
 
 export default themeReducer;
