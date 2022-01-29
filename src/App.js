e27d5e279cd55619b77de4414a30cbac18ad225b
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-29 14:47:25 +0545

    :construction: working on navbar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setUser } from "./reducers/userReducer";
 import { fetchPosts } from "./reducers/postReducer";
 import { setSubList, setTopSubsList } from "./reducers/subReducer";
 import { setDarkMode } from "./reducers/themeReducer";
 import { notify } from "./reducers/notificationReducer";
 import getErrorMsg from "./utils/getErrorMsg";
+import { Paper } from "@material-ui/core/";
+import customTheme from "./styles/customTheme";
+import { useMainPaperStyles } from "./styles/muiStyles";
+import { ThemeProvider } from "@material-ui/core/styles";
 
 const App = () => {
-    const dispatch = useDispatch();
-    useEffect(() => {
-      const setPostsAndSubreddits = async () => {
-        try {
-          await dispatch(fetchPosts("hot"));
-          await dispatch(setSubList());
-          await dispatch(setTopSubsList());
-        } catch (err) {
-          dispatch(notify(getErrorMsg(err), "error"));
-        }
-      };
+  const dispatch = useDispatch();
+  const classes = useMainPaperStyles();
+  const { darkMode } = useSelector((state) => state);
 
-      dispatch(setUser());
-      dispatch(setDarkMode());
-      setPostsAndSubreddits();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-  return <></>;
+  useEffect(() => {
+    const setPostsAndSubreddits = async () => {
+      try {
+        await dispatch(fetchPosts("hot"));
+        await dispatch(setSubList());
+        await dispatch(setTopSubsList());
+      } catch (err) {
+        dispatch(notify(getErrorMsg(err), "error"));
+      }
+    };
+
+    dispatch(setUser());
+    dispatch(setDarkMode());
+    setPostsAndSubreddits();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <ThemeProvider theme={customTheme(darkMode)}>
+      <Paper className={classes.root} elevation={0}></Paper>
+    </ThemeProvider>
+  );
 };
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  useMediaQuery,
+  IconButton,
+} from "@material-ui/core";
+
+
+const Navbar = () => [
+    
+]
+export default Navbar;
