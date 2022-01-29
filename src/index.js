3432e39aceee5d251de5eadeb0fc737b3f59621c
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-29 14:12:12 +0545

    :fire: provider and store threaded in index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./style.css";
-import {BrowserRouter} from "react-router-dom";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import "typeface-varela-round";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
   document.getElementById("root")
 );
