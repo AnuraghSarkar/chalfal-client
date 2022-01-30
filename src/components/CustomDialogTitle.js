d9b7e8f83dee1ed38898e5498d2632411b9dfc40
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-30 10:55:34 +0545

    :fire: importing necessary module

--- src/components/CustomDialogTitle.js
+++ src/components/CustomDialogTitle.js
@@ -1,3 +1,11 @@
+import React from "react";
+import { Typography, IconButton } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import MuiDialogTitle from "@material-ui/core/DialogTitle";
+import MuiDialogContent from "@material-ui/core/DialogContent";
+import MuiDialogActions from "@material-ui/core/DialogActions";
+import CloseIcon from "@material-ui/icons/Close";
+
 const styles = (theme) => ({
   root: {
     margin: 0,
