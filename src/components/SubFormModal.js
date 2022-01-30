48ad8d8b992c8b03a14b1439796f8232defef166
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-30 10:58:33 +0545

    :clown_face: working on sub modal form

--- src/components/SubFormModal.js
+++ src/components/SubFormModal.js
@@ -1,5 +1,33 @@
-import React, { useState } from 'react'
-import SubForm from './SubForm'
+import React, { useState } from "react";
+import SubForm from "./SubForm";
+import { DialogTitle } from "./CustomDialogTitle";
 
-const SubFormModal = () => { };
+import {
+  Dialog,
+  DialogContent,
+  Button,
+  MenuItem,
+  ListItemIcon,
+} from "@material-ui/core";
+import { useDialogStyles } from "../styles/muiStyles";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+
+const SubFormModal = ({ type, handleCloseMenu }) => {
+  const classes = useDialogStyles();
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpenMenu = () => {
+    handleClickOpen();
+    handleCloseMenu();
+  };
+  return <></>;
+};
 export default SubFormModal;
