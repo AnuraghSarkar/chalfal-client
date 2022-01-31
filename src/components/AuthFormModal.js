18c2ff3f9a4e656cde4860070a8e8473725812e9
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-31 19:37:57 +0545

    :hammer: wreakpointing mobile view

--- src/components/AuthFormModal.js
+++ src/components/AuthFormModal.js
@@ -19,6 +19,27 @@ import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
-const AuthFormModal = () => {};
+const AuthFormModal = ({ closeMobileMenu, type }) => {
+  const classes = useDialogStyles();
+  const classesBtn = useNavStyles();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleMobileMenu = () => {
+    handleClickOpen();
+    closeMobileMenu();
+  };
+
+  return <></>;
+};
 
 export default AuthFormModal;
