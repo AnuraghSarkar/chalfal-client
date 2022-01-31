370413955daa653f509f19d6c9c444a53a2a6849
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-31 19:36:49 +0545

    :hammer: importing necessary modules

--- src/components/AuthFormModal.js
+++ src/components/AuthFormModal.js
@@ -1,9 +1,24 @@
 import { useState } from "react";
-import PropTypes from 'prop-types';
-import AuthForm from './AuthForm';
+import PropTypes from "prop-types";
+import AuthForm from "./AuthForm";
+import { DialogTitle } from "./CustomDialogTitle";
 
-const AuthFormModal = () => {
-    
-}
+import {
+  Dialog,
+  DialogContent,
+  Button,
+  IconButton,
+  MenuItem,
+  useMediaQuery,
+  ListItemIcon,
+} from "@material-ui/core";
+import { useDialogStyles } from "../styles/muiStyles";
+import { useTheme } from "@material-ui/core/styles";
+import { useNavStyles } from "../styles/muiStyles";
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+
+const AuthFormModal = () => {};
 
 export default AuthFormModal;
