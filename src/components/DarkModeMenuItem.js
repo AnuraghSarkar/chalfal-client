fbd9d939ef9462c6452e9b3a1d06945661699ee8
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-02-01 17:29:51 +0545

    handling states and dispatcher

--- src/components/DarkModeMenuItem.js
+++ src/components/DarkModeMenuItem.js
@@ -1,4 +1,22 @@
 import React from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { toggleDarkMode } from "../reducers/themeReducer";
 
-const DarkModeMenuItem = ({ closeMenu, navItem }) => { }
+const DarkModeMenuItem = ({ closeMenu, navItem }) => {
+    const { darkMode } = useSelector((state) => state);
+    const dispatch = useDispatch();
+
+    const handleDarkMode = () => {
+        dispatch(toggleDarkMode(!darkMode));
+        closeMenu();
+    };
+
+    if (navItem) {
+        return (
+            <IconButton color="primary" onClick={handleDarkMode}>
+                {darkMode ? <Brightness4Icon /> : <Brightness7Icon />}
+            </IconButton>
+        );
+    }
+};
 export default DarkModeMenuItem;
