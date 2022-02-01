dbc2ef44901b66c7ead5a76482e5975dfb76b25c
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-02-01 17:24:11 +0545

    :clown_face: handling setstate for avatar form

--- src/components/UpdateAvatarModal.js
+++ src/components/UpdateAvatarModal.js
@@ -10,3 +10,19 @@ import {
 } from "@material-ui/core";
 import { useDialogStyles } from "../styles/muiStyles";
 import FaceIcon from "@material-ui/icons/Face";
+
+
+const UpdateAvatarModal = ({ handleCloseMenu, user }) => {
+    const classes = useDialogStyles();
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+        handleCloseMenu();
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+}
+export default UpdateAvatarModal;
