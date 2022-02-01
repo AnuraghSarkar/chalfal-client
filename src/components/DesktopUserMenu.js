f9fc5c36b95b38fc4d776f2bf4b9309328406e73
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-02-01 17:33:01 +0545

    :fire: handling state for destop menu

--- src/components/DesktopUserMenu.js
+++ src/components/DesktopUserMenu.js
@@ -1,4 +1,44 @@
 import React, { useState } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import AuthFormModal from "./AuthFormModal";
+import SubFormModal from "./SubFormModal";
+import UpdateAvatarModal from "./UpdateAvatarModal";
+import DarkModeMenuItem from "./DarkModeMenuItem";
+import { getCircularAvatar } from "../utils/cloudinaryTransform";
+import storageService from "../utils/localStorage";
 
-const DesktopUserMenu = ({ user, handleLogout }) => {};
+import {
+  Button,
+  Menu,
+  MenuItem,
+  Avatar,
+  Typography,
+  ListItemIcon,
+  Divider,
+} from "@material-ui/core";
+import { useUserMenuStyles } from "../styles/muiStyles";
+import FilterVintageIcon from "@material-ui/icons/FilterVintage";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
+
+const DesktopUserMenu = ({ user, handleLogout }) => {
+  const classes = useUserMenuStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogoutClick = () => {
+    handleClose();
+    handleLogout();
+  };
+
+  const loggedUser = storageService.loadUser() || user;
+};
+
 export default DesktopUserMenu;
