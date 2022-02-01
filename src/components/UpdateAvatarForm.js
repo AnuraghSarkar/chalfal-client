1b30032ef0b092c82477232fac6ddc6e736b13ce
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-02-01 17:23:31 +0545

    :clown_face: importing required modules

--- src/components/UpdateAvatarForm.js
+++ src/components/UpdateAvatarForm.js
@@ -157,3 +157,5 @@ const UpdateAvatarForm = ({ closeModal }) => {
     </div>
   );
 };
+
+export default UpdateAvatarForm;
---
+++ src/components/UpdateAvatarModal.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import UpdateAvatarForm from "./UpdateAvatarForm";
+
+import { DialogTitle } from "./CustomDialogTitle";
+import {
+  Dialog,
+  DialogContent,
+  MenuItem,
+  ListItemIcon,
+} from "@material-ui/core";
+import { useDialogStyles } from "../styles/muiStyles";
+import FaceIcon from "@material-ui/icons/Face";
