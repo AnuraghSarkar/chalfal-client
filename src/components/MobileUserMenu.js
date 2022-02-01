66d720cb0db0387f55d1e7fdb9d15fc3ccb4d423
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-02-01 17:48:58 +0545

    :fire: adding user icon on menu bar

--- src/components/MobileUserMenu.js
+++ src/components/MobileUserMenu.js
@@ -7,6 +7,20 @@ import DarkModeMenuItem from "./DarkModeMenuItem";
 import { getCircularAvatar } from "../utils/cloudinaryTransform";
 import storageService from "../utils/localStorage";
 
+import {
+  IconButton,
+  Menu,
+  MenuItem,
+  Avatar,
+  ListItemIcon,
+  Divider,
+} from '@material-ui/core';
+import { useUserMenuStyles } from '../styles/muiStyles';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+
+
 const MobileUserMenu = ({ user, handleLogout }) => {
     const [anchorEl, setAnchorEl] = useState(null);
     const classes = useUserMenuStyles();
@@ -26,6 +40,40 @@ const MobileUserMenu = ({ user, handleLogout }) => {
 
     const loggedUser = storageService.loadUser() || user;
 
+    return (
+    <div>
+      {loggedUser ? (
+        <IconButton onClick={handleMenu} className={classes.userBtnMob}>
+          {loggedUser?.avatar?.exists ? (
+            <Avatar
+              alt={loggedUser.username}
+              src={getCircularAvatar(loggedUser.avatar.imageLink)}
+              className={classes.avatar}
+            />
+          ) : (
+            <Avatar className={classes.avatar}>{loggedUser.username[0]}</Avatar>
+          )}
+          <MoreVertIcon color="primary" />
+        </IconButton>
+      ) : (
+        <IconButton onClick={handleMenu} color="primary">
+          <MoreVertIcon color="primary" />
+        </IconButton>
+      )}
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      ></Menu>
 };
 
 export default MobileUserMenu;
