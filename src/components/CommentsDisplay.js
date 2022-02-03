67649bc18591f756278a17bdce8981e60249e175
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-02-03 20:05:04 +0545

    :hammer: setting state for handling reply downvote

--- src/components/CommentsDisplay.js
+++ src/components/CommentsDisplay.js
@@ -19,111 +19,136 @@ import { usePostCommentsStyles } from "../styles/muiStyles";
 import ForumIcon from "@material-ui/icons/Forum";
 
 const CommentsDisplay = ({ comments, postId, isMobile }) => {
-      const classes = usePostCommentsStyles();
-      const dispatch = useDispatch();
-      const user = useSelector((state) => state.user);
+  const classes = usePostCommentsStyles();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
 
-      const handleCommentUpvote = async (commentId) => {
-        const { upvotedBy, downvotedBy } = comments.find(
-          (c) => c.id === commentId
+  const handleCommentUpvote = async (commentId) => {
+    const { upvotedBy, downvotedBy } = comments.find((c) => c.id === commentId);
+
+    try {
+      if (upvotedBy.includes(user.id)) {
+        const updatedUpvotedBy = upvotedBy.filter((u) => u !== user.id);
+        dispatch(
+          toggleCommentUpvote(postId, commentId, updatedUpvotedBy, downvotedBy)
         );
-
-        try {
-          if (upvotedBy.includes(user.id)) {
-            const updatedUpvotedBy = upvotedBy.filter((u) => u !== user.id);
-            dispatch(
-              toggleCommentUpvote(
-                postId,
-                commentId,
-                updatedUpvotedBy,
-                downvotedBy
-              )
-            );
-          } else {
-            const updatedUpvotedBy = [...upvotedBy, user.id];
-            const updatedDownvotedBy = downvotedBy.filter((d) => d !== user.id);
-            dispatch(
-              toggleCommentUpvote(
-                postId,
-                commentId,
-                updatedUpvotedBy,
-                updatedDownvotedBy
-              )
-            );
-          }
-        } catch (err) {
-          dispatch(notify(getErrorMsg(err), "error"));
-        }
-    };
-    const handleCommentDownvote = async (commentId) => {
-      const { upvotedBy, downvotedBy } = comments.find(
-        (c) => c.id === commentId
-      );
-
-      try {
-        if (downvotedBy.includes(user.id)) {
-          const updatedDownvotedBy = downvotedBy.filter((d) => d !== user.id);
-          dispatch(
-            toggleCommentDownvote(
-              postId,
-              commentId,
-              updatedDownvotedBy,
-              upvotedBy
-            )
-          );
-        } else {
-          const updatedDownvotedBy = [...downvotedBy, user.id];
-          const updatedUpvotedBy = upvotedBy.filter((u) => u !== user.id);
-          dispatch(
-            toggleCommentDownvote(
-              postId,
-              commentId,
-              updatedDownvotedBy,
-              updatedUpvotedBy
-            )
-          );
-        }
-      } catch (err) {
-        dispatch(notify(getErrorMsg(err), "error"));
+      } else {
+        const updatedUpvotedBy = [...upvotedBy, user.id];
+        const updatedDownvotedBy = downvotedBy.filter((d) => d !== user.id);
+        dispatch(
+          toggleCommentUpvote(
+            postId,
+            commentId,
+            updatedUpvotedBy,
+            updatedDownvotedBy
+          )
+        );
       }
-    };
+    } catch (err) {
+      dispatch(notify(getErrorMsg(err), "error"));
+    }
+  };
+  const handleCommentDownvote = async (commentId) => {
+    const { upvotedBy, downvotedBy } = comments.find((c) => c.id === commentId);
 
-      const handleReplyUpvote = async (commentId, replyId) => {
-        const targetComment = comments.find((c) => c.id === commentId);
-        const { upvotedBy, downvotedBy } = targetComment.replies.find(
-          (r) => r.id === replyId
+    try {
+      if (downvotedBy.includes(user.id)) {
+        const updatedDownvotedBy = downvotedBy.filter((d) => d !== user.id);
+        dispatch(
+          toggleCommentDownvote(
+            postId,
+            commentId,
+            updatedDownvotedBy,
+            upvotedBy
+          )
         );
+      } else {
+        const updatedDownvotedBy = [...downvotedBy, user.id];
+        const updatedUpvotedBy = upvotedBy.filter((u) => u !== user.id);
+        dispatch(
+          toggleCommentDownvote(
+            postId,
+            commentId,
+            updatedDownvotedBy,
+            updatedUpvotedBy
+          )
+        );
+      }
+    } catch (err) {
+      dispatch(notify(getErrorMsg(err), "error"));
+    }
+  };
 
-        try {
-          if (upvotedBy.includes(user.id)) {
-            const updatedUpvotedBy = upvotedBy.filter((u) => u !== user.id);
-            dispatch(
-              toggleReplyUpvote(
-                postId,
-                commentId,
-                replyId,
-                updatedUpvotedBy,
-                downvotedBy
-              )
-            );
-          } else {
-            const updatedUpvotedBy = [...upvotedBy, user.id];
-            const updatedDownvotedBy = downvotedBy.filter((d) => d !== user.id);
-            dispatch(
-              toggleReplyUpvote(
-                postId,
-                commentId,
-                replyId,
-                updatedUpvotedBy,
-                updatedDownvotedBy
-              )
-            );
-          }
-        } catch (err) {
-          dispatch(notify(getErrorMsg(err), "error"));
-        }
-      };
+  const handleReplyUpvote = async (commentId, replyId) => {
+    const targetComment = comments.find((c) => c.id === commentId);
+    const { upvotedBy, downvotedBy } = targetComment.replies.find(
+      (r) => r.id === replyId
+    );
 
+    try {
+      if (upvotedBy.includes(user.id)) {
+        const updatedUpvotedBy = upvotedBy.filter((u) => u !== user.id);
+        dispatch(
+          toggleReplyUpvote(
+            postId,
+            commentId,
+            replyId,
+            updatedUpvotedBy,
+            downvotedBy
+          )
+        );
+      } else {
+        const updatedUpvotedBy = [...upvotedBy, user.id];
+        const updatedDownvotedBy = downvotedBy.filter((d) => d !== user.id);
+        dispatch(
+          toggleReplyUpvote(
+            postId,
+            commentId,
+            replyId,
+            updatedUpvotedBy,
+            updatedDownvotedBy
+          )
+        );
+      }
+    } catch (err) {
+      dispatch(notify(getErrorMsg(err), "error"));
+    }
+  };
+  const handleReplyDownvote = async (commentId, replyId) => {
+    const targetComment = comments.find((c) => c.id === commentId);
+    const { upvotedBy, downvotedBy } = targetComment.replies.find(
+      (r) => r.id === replyId
+    );
+
+    try {
+      if (downvotedBy.includes(user.id)) {
+        const updatedDownvotedBy = downvotedBy.filter((d) => d !== user.id);
+        dispatch(
+          toggleReplyDownvote(
+            postId,
+            commentId,
+            replyId,
+            updatedDownvotedBy,
+            upvotedBy
+          )
+        );
+      } else {
+        const updatedDownvotedBy = [...downvotedBy, user.id];
+        const updatedUpvotedBy = upvotedBy.filter((u) => u !== user.id);
+        dispatch(
+          toggleReplyDownvote(
+            postId,
+            commentId,
+            replyId,
+            updatedDownvotedBy,
+            updatedUpvotedBy
+          )
+        );
+      }
+    } catch (err) {
+      dispatch(notify(getErrorMsg(err), "error"));
+    }
+  };
 };
 
 export default CommentsDisplay;
