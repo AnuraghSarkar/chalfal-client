d9f4d2b61e4fb8d0f10e63ab74cf8710ae6b605d
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-02-03 19:56:41 +0545

    :fire: component for comment input created

--- src/components/CommentInput.js
+++ src/components/CommentInput.js
@@ -27,6 +27,47 @@ const CommentInput = ({ user, postId, isMobile }) => {
       dispatch(notify(getErrorMsg(err), "error"));
     }
   };
+  return (
+    <div className={classes.wrapper}>
+      {user ? (
+        <Typography variant="body2">
+          Comment as{" "}
+          <Link component={RouterLink} to={`/u/${user.username}`}>
+            {user.username}
+          </Link>
+        </Typography>
+      ) : (
+        <Typography variant="body1">
+          Log in or sign up to leave a comment
+        </Typography>
+      )}
+      <form className={classes.form} onSubmit={handlePostComment}>
+        <TextField
+          placeholder={`What are your thoughts?`}
+          multiline
+          fullWidth
+          required
+          rows={4}
+          rowsMax={Infinity}
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          variant="outlined"
+          size={isMobile ? "small" : "medium"}
+        />
+        <Button
+          type="submit"
+          color="primary"
+          variant="contained"
+          className={classes.commentBtn}
+          startIcon={<SendIcon />}
+          size={isMobile ? "small" : "medium"}
+          disabled={!user || submitting}
+        >
+          {!user ? "Login to comment" : submitting ? "Commenting" : "Comment"}
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default CommentInput;
