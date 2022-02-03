b0a90b57feb7985ca515d54d61156e67ef7a807b
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-02-03 19:59:12 +0545

    :hammer: handling post reply

--- src/components/CommentAndButtons.js
+++ src/components/CommentAndButtons.js
@@ -1,6 +1,42 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
+import {
+  addReply,
+  editComment,
+  deleteComment,
+} from "../reducers/postCommentsReducer";
+import { notify } from "../reducers/notificationReducer";
+import DeleteDialog from "./DeleteDialog";
+import getErrorMsg from "../utils/getErrorMsg";
 
-const CommentAndButtons = ({ isMobile, comment, postId, user }) => { };
+import { TextField, Button, Typography } from "@material-ui/core";
+import { useCommentAndBtnsStyles } from "../styles/muiStyles";
+import ReplyIcon from "@material-ui/icons/Reply";
+import SendIcon from "@material-ui/icons/Send";
+import EditIcon from "@material-ui/icons/Edit";
+
+const CommentAndButtons = ({ isMobile, comment, postId, user }) => {
+  const classes = useCommentAndBtnsStyles();
+  const dispatch = useDispatch();
+  const [replyOpen, setReplyOpen] = useState(false);
+  const [editOpen, setEditOpen] = useState(false);
+  const [replyInput, setReplyInput] = useState("");
+  const [editInput, setEditInput] = useState(comment.commentBody);
+  const [submitting, setSubmitting] = useState(false);
+
+  const handlePostReply = async () => {
+    try {
+      setSubmitting(true);
+      await dispatch(addReply(postId, comment.id, replyInput));
+      setSubmitting(false);
+      setReplyOpen(false);
+      setReplyInput("");
+      dispatch(notify(`Reply submitted!`, "success"));
+    } catch (err) {
+      setSubmitting(false);
+      dispatch(notify(getErrorMsg(err), "error"));
+    }
+  };
+};
 
 export default CommentAndButtons;
