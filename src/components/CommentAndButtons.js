1196f9a2296bf945c2eb0de09faa23027f7ce17e
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-02-03 19:58:30 +0545

    :hammer: working on comment and buttons ui

---
+++ src/components/CommentAndButtons.js
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+
+const CommentAndButtons = ({ isMobile, comment, postId, user }) => { };
+
+export default CommentAndButtons;
