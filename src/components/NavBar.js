a1204842ba21c62b70a52ef17fdd896b8b125c2b
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-02-03 19:52:26 +0545

    :fire: finished navbar mobile support!

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,13 +16,6 @@ import {
   IconButton,
 } from "@material-ui/core";
 
-import {
-  AppBar,
-  Toolbar,
-  Button,
-  useMediaQuery,
-  IconButton,
-} from "@material-ui/core";
 import { useNavStyles } from "../styles/muiStyles";
 import { useTheme } from "@material-ui/core/styles";
 import RedditIcon from "@material-ui/icons/Reddit";
@@ -33,31 +26,56 @@ const Navbar = () => {
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
+  const classes = useNavStyles();
 
-    const handleLogout = () => { };
-    
-    return (
-      <AppBar position="sticky" color="inherit" elevation={1}>
-        <Toolbar>
-          {!searchOpen && (
-            <>
-              <div className={classes.leftPortion}>
-                <div className={classes.logoWrapper}>
-                  <Button
-                    className={classes.logo}
-                    color="primary"
-                    component={RouterLink}
-                    to="/"
-                    startIcon={<RedditIcon fontSize="large" />}
-                    size="large"
-                  >
-                    chalfal
-                  </Button>
-                </div>
-          )}
-        </Toolbar>
-      </AppBar>
-    );
+  const handleLogout = () => {
+    dispatch(logoutUser());
+    dispatch(notify(`u/${user.username} logged out`, "success"));
+  };
+
+  return (
+    <AppBar position="sticky" color="inherit" elevation={1}>
+      <Toolbar disableGutters={isMobile}>
+        {!searchOpen && (
+          <>
+            <div className={classes.leftPortion}>
+              <div className={classes.logoWrapper}>
+                <Button
+                  className={classes.logo}
+                  color="primary"
+                  component={RouterLink}
+                  to="/"
+                  startIcon={<RedditIcon fontSize="large" />}
+                  size="large"
+                >
+                  chalfal
+                </Button>
+              </div>
+              {!isMobile && <SearchBar />}
+            </div>
+            {isMobile ? (
+              <>
+                <IconButton
+                  color="primary"
+                  className={classes.searchBtn}
+                  onClick={() => setSearchOpen((prevState) => !prevState)}
+                >
+                  <SearchIcon />
+                </IconButton>
+                <MobileUserMenu user={user} handleLogout={handleLogout} />
+              </>
+            ) : (
+              <DesktopUserMenu user={user} handleLogout={handleLogout} />
+            )}
+          </>
+        )}
+        {searchOpen && isMobile && (
+          <SearchBar isMobile={true} setSearchOpen={setSearchOpen} />
+        )}
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Navbar;
